9ebdde9af1498404d284707d88e0a7fba9122cac
dxtobi <akanbijosephtobi@gmail>
2023-08-16 00:30:00 -0700

    added transitions

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,11 +1,13 @@
 
 
-export const load = async ({locals}) => {
+export const load = async ({locals, url}) => {
   //console.log(locals.user)
 const cart = await locals.CART.findOne({ customer: locals.customer?._id }).populate('items.food')
  
   return {
     customer: locals.customer,
-    cartItem:cart ? cart.items?.length : 0
+    cartItem: cart ? cart.items?.length : 0,
+    url: url.pathname,
   }
 };
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script>
-	import { navigating } from '$app/stores';
+	import { fly } from 'svelte/transition'
+
 	import PageLoader from './../components/loaders/PageLoader.svelte';
 	import Header from '../components/layouts/Header.svelte';
 	import './styles.css';
 	import Analytics from '../components/Analytics.svelte';
     
 	
-	
+	export let data
 	
 	
 
@@ -16,11 +17,16 @@
 <div class="w-full">
 	<Header />
 	<PageLoader/>
+	{#key data.url}
+		<main
+		in:fly={{ x: -200, duration: 300, delay: 300 }}
+		out:fly={{ x: 200, duration: 300 }}
+		 class="w-full py-16 md:w-1/2 m-auto">
+			
+			<slot />
+		</main>
+	{/key}
 	
-	<main class="w-full py-16 md:w-1/2 m-auto">
-		
-		<slot />
-	</main>
 
 	<footer>
 		
